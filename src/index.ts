846953b12f1782e869c0dada09fec80c64ce6d6e
Timi16 <timilehinolowu46@gmail.com>
2025-09-07 00:36:47 +0100

    adding index file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-
+// src/index.ts
 export * from "./types";
 export * from "./schema/inft-manifest-schema";
 export * from "./utils/base64";
 export * from "./utils/cosine";
+export * from "./utils/fpI6";
+export * from "./utils/uri";
+export * from "./services/binary.vector";
+export * from "./services/ipfs.service";
 export * from "./services/precomputed-store";
+export * from "./services/external-store";
 export * from "./services/embedding.service";
 export * from "./services/inft-loader-service";
